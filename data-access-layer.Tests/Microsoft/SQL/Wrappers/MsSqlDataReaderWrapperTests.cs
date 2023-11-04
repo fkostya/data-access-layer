85cf001f6db6e5c91b70ef11529d889a978ce0bb
kostya <froimovich@gmail.com>
2023-11-03 22:33:06 -0400

    unit test

---
+++ data-access-layer.Tests/Microsoft/SQL/Wrappers/MsSqlDataReaderWrapperTests.cs
@@ -0,0 +1,251 @@
+﻿using data_access_layer.Microsoft.SQL.Wrappers;
+using Moq;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Common;
+
+namespace data_access_layer.Tests.Microsoft.SQL
+{
+    public class MsSqlDataReaderWrapperTests
+    {
+        private readonly Mock<DbDataReader> reader = new();
+
+        public class DbColumnStub(string columnName) : DbColumn
+        {
+            public new string ColumnName { get; set; } = columnName;
+        }
+
+        [Fact]
+        public async Task GetColumnSchemaAsync_NoReaderProvided_EmptyColumns()
+        {
+            MsSqlDataReaderWrapper wrapper = new();
+            var columns = await wrapper.GetColumnSchemaAsync();
+
+            Assert.Empty(columns);
+        }
+
+        [Fact]
+        public async Task GetColumnSchemaAsync_DbReaderProvided_NotEmptyColumns()
+        {
+            reader
+                .Setup(_ => _.GetColumnSchemaAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ReadOnlyCollection<DbColumn>(new List<DbColumn>() { new DbColumnStub("column-0") }));
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+            var columns = await wrapper.GetColumnSchemaAsync();
+
+            Assert.NotEmpty(columns);
+            Assert.Equal("column-0", (columns?.FirstOrDefault() as DbColumnStub)?.ColumnName);
+        }
+
+        [Fact]
+        public async Task NextResultAsync_DbReaderProvided_HasNextResultTrue()
+        {
+            reader
+                .Setup(_ => _.NextResultAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+            var next = await wrapper.NextResultAsync();
+
+            Assert.True(next);
+        }
+
+        [Fact]
+        public async Task NextResultAsync_DbReaderProvided_HasLastResultFalse()
+        {
+            reader
+                .SetupSequence(_ => _.NextResultAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true)
+                .ReturnsAsync(false);
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+            var next = await wrapper.NextResultAsync();
+            var last = await wrapper.NextResultAsync();
+
+            Assert.False(last);
+        }
+
+        [Fact]
+        public async Task NextResultAsync_NoDbReaderProvided_HasNextResultFalse()
+        {
+            reader
+                .Setup(_ => _.NextResultAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            MsSqlDataReaderWrapper wrapper = new();
+            var next = await wrapper.NextResultAsync();
+
+            Assert.False(next);
+        }
+
+        [Fact]
+        public void GetSchemaTable_NoDbReaderProvided_EmptyDataTable()
+        {
+            reader
+                .Setup(_ => _.GetSchemaTable())
+                .Returns(new DataTable());
+
+            MsSqlDataReaderWrapper wrapper = new();
+            var db = wrapper.GetSchemaTable();
+
+            Assert.NotNull(db);
+        }
+
+        [Fact]
+        public void GetSchemaTable_DbReaderProvided_NotEmptyDataTable()
+        {
+            var table = new DataTable("unit-test-table-0");
+
+            reader
+                .Setup(_ => _.GetSchemaTable())
+                .Returns(table);
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+            var db = wrapper.GetSchemaTable();
+
+            Assert.NotNull(db);
+            Assert.Equal("unit-test-table-0", db.TableName);
+        }
+
+        [Fact]
+        public async Task ReadAsync_NoDbReaderProvided_NoReadFalse()
+        {
+            reader
+                .Setup(_ => _.ReadAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            MsSqlDataReaderWrapper wrapper = new();
+            var read = await wrapper.ReadAsync();
+
+            Assert.False(read);
+        }
+
+        [Fact]
+        public async Task ReadAsync_DbReaderProvided_HasReadTrue()
+        {
+            reader
+                .Setup(_ => _.ReadAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+            var read = await wrapper.ReadAsync();
+
+            Assert.True(read);
+        }
+
+        [Fact]
+        public void GetValue_NoDbReaderProvided_ObjectAsNull()
+        {
+            MsSqlDataReaderWrapper wrapper = new();
+            var value = wrapper.GetValueOrDefault(It.IsAny<string>());
+
+            Assert.Null(value);
+        }
+
+        [Fact]
+        public void GetValue_NoDbReaderProvided_ObjectAsEmpty()
+        {
+            MsSqlDataReaderWrapper wrapper = new();
+            var value = wrapper.GetValueOrDefault(It.IsAny<string>(), new object());
+
+            Assert.NotNull(value);
+        }
+
+        [Fact]
+        public void GetValue_DbReaderProvided_ObjectAsEmpty()
+        {
+            reader
+                .Setup(_ => _.GetValue(It.IsAny<int>()))
+                .Returns(new object());
+            reader
+                .Setup(_ => _.GetOrdinal(It.IsAny<string>()))
+                .Returns(0);
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+            var value = wrapper.GetValueOrDefault(It.IsAny<string>());
+
+            Assert.NotNull(value);
+        }
+
+        [Fact]
+        public void Close_NoDbReaderProvided_NoException()
+        {
+            MsSqlDataReaderWrapper wrapper = new();
+
+            wrapper.Close();
+        }
+
+        [Fact]
+        public void Close_DbReaderProvided_CloseIsCalledOnce()
+        {
+            reader
+                .Setup(x => x.Close())
+                .Verifiable();
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+
+            wrapper.Close();
+        }
+
+        [Fact]
+        public async Task DisposeAsync_NoDbReaderProvided_DisposeAsyncCalledOnce()
+        {
+            reader
+                .Setup(x => x.DisposeAsync())
+                .Verifiable();
+
+            MsSqlDataReaderWrapper wrapper = new();
+
+            await wrapper.DisposeAsync();
+        }
+
+        [Fact]
+        public void HasRow_NoDbReaderProvided_NoRowsFalse()
+        {
+            MsSqlDataReaderWrapper wrapper = new();
+
+            var noRows = wrapper.HasRows;
+
+            Assert.False(noRows);
+        }
+
+        [Fact]
+        public void HasRow_DbReaderProvided_HasRowsTrue()
+        {
+            reader
+                .Setup(x => x.HasRows)
+                .Returns(true);
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+
+            var hasRows = wrapper.HasRows;
+
+            Assert.True(hasRows);
+        }
+
+        [Fact]
+        public void Index_NoDbReaderProvided_EmptyObject()
+        {
+            MsSqlDataReaderWrapper wrapper = new();
+
+            var value = wrapper[It.IsAny<string>()];
+
+            Assert.NotNull(value);
+        }
+
+        [Fact]
+        public void Index_DbReaderProvided_EmptyObject()
+        {
+            reader
+                .Setup(x => x[It.IsAny<string>()])
+                .Returns(new object());
+
+            MsSqlDataReaderWrapper wrapper = new(reader.Object);
+
+            var value = wrapper[It.IsAny<string>()];
+
+            Assert.NotNull(value);
+        }
+    }
+}
--- data-access-layer/Microsoft/SQL/Wrappers/MsSqlDataReaderWrapper.cs
+++ data-access-layer/Microsoft/SQL/Wrappers/MsSqlDataReaderWrapper.cs
@@ -8,11 +8,11 @@ namespace data_access_layer.Microsoft.SQL.Wrappers
     [ExcludeFromCodeCoverage]
     public class MsSqlDataReaderWrapper : IAsyncDisposable
     {
-        private readonly DbDataReader _reader;
+        private readonly DbDataReader? _reader;
         private bool empty = false;
 
         #region ctor
-        public MsSqlDataReaderWrapper(DbDataReader reader)
+        public MsSqlDataReaderWrapper(DbDataReader? reader)
         {
             _reader = reader;
         }
@@ -24,21 +24,25 @@ namespace data_access_layer.Microsoft.SQL.Wrappers
         }
         #endregion
 
-        public virtual bool HasRows => _reader.HasRows;
-        public virtual object this[string key] => _reader[key];
+        public virtual bool HasRows => _reader?.HasRows ?? false;
+        public virtual object this[string key] => _reader?[key] ?? new object();
 
         public ValueTask DisposeAsync()
         {
-            return _reader.DisposeAsync();
+            return _reader?.DisposeAsync() ?? ValueTask.CompletedTask;
         }
 
         public DataTable? GetSchemaTable()
         {
+            if (_reader == null) return new DataTable();
+
             return _reader.GetSchemaTable();
         }
 
         public Task<bool> ReadAsync(CancellationToken cancellationToken = default)
         {
+            if(_reader == null) return Task.FromResult(false);
+
             return _reader.ReadAsync(cancellationToken);
         }
 
@@ -46,8 +50,7 @@ namespace data_access_layer.Microsoft.SQL.Wrappers
         {
             try
             {
-                //return reader.GetOrdinal(key) >= 0 ? reader.GetValue(key) : @default;
-                return _reader.GetValue(key);
+                return _reader?.GetValue(key) ?? @default;
             }
             catch
             {
@@ -58,6 +61,7 @@ namespace data_access_layer.Microsoft.SQL.Wrappers
 
         public async Task<ReadOnlyCollection<DbColumn>> GetColumnSchemaAsync(CancellationToken cancellationToken = default)
         {
+            if (_reader == null) return await Task.FromResult(new ReadOnlyCollection<DbColumn>(new List<DbColumn>()));
             try
             {
                 return await _reader.GetColumnSchemaAsync();
@@ -71,12 +75,14 @@ namespace data_access_layer.Microsoft.SQL.Wrappers
 
         public Task<bool> NextResultAsync(CancellationToken cancellationToken = default)
         {
+            if (_reader == null) return Task.FromResult(false);
+
             return _reader.NextResultAsync(cancellationToken);
         }
 
         public void Close()
         {
-            _reader.Close();
+            _reader?.Close();
         }
     }
 }
